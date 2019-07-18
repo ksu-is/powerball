724d8ef600a6f1a97200a4bb7efee5a612e96ab2
diroboto <danieldiroberto@onetechnology.com>
2019-07-18 17:44:50 -0400

    Update powerball.py

--- powerball.py
+++ powerball.py
@@ -1,18 +1,16 @@
-#!/usr/bin/env python
 
-"""
-If you happen to win, I only ask for a measly 1% tip. ;)
-To run, save this file and run it by typing the following in your terminal:
-python powerball.py
-"""
+
+
 from pprint import pprint as pp
 import random
 
 def powerball():
     drum1 = list(range(1,70))
     drum2 = list(range(1,27))
+    number_even = (random.randint(0,70)) * 2
+    number_odd = (random.randint(0,70)) * 2 +1
     white_balls = []
-    
+
     for _ in range(5):
         choice = random.SystemRandom().choice(drum1)
         drum1.pop(drum1.index(choice))
@@ -21,8 +19,11 @@ def powerball():
     return white_balls + [random.SystemRandom().choice(drum2)]
     
 try:
-    budget = int(int(input('What is your powerball budget? (Number must be multiple of 2)'))/2)
+    budget = int(int(input('How much money would you like to spend on tickets? (Each ticket cost $2)'))/2)
+    
 except:
     budget = 1
 
 pp(list(powerball() for i in range(budget)))
+
+
